26503d095c12b1c4cea6bbcf0e6852de49a2626e
jcastellanosierras <jcastellanosierras@gmail.com>
2024-02-10 18:20:26 +0100

    feat: add vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://josecastellano.dev',
-  integrations: [sitemap()]
+  integrations: [sitemap(), mdx()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  })
 });
